8b5fd109dd1e57e17a53570b8f5267716585c711
innabodak <innabodak@ukr.net>
2019-01-21 03:11:58 +0200

    передача данных через props

---
+++ source/components/Post/index.js
@@ -0,0 +1,27 @@
+// Core
+import React, { Component } from 'react';
+import moment from 'moment';
+
+//Instruments
+import Styles from './styles.m.css';
+
+export default class Post extends Component {
+    render() {
+        const {
+            avatar,
+            currentUserFirstName,
+            currentUserLastName,
+        } = this.props;
+
+        return (
+            <section className = { Styles.post }>
+                <img src = { avatar } />
+                <a>
+                    { `${ currentUserFirstName } ${ currentUserLastName } `}
+                </a>
+                <time> { moment().format('MMM D h:mm:ss a') } </time>
+                <p>Hello</p>
+            </section>
+        );
+    }
+}
