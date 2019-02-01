40c76cded9578eea0672dd402b5f92e9c3436ef5
innabodak <innabodak@ukr.net>
2019-02-01 23:43:22 +0200

    HOC refactoring

--- source/components/Like/index.js
+++ source/components/Like/index.js
@@ -3,86 +3,75 @@ import React, { Component } from 'react';
 import { func, string, arrayOf, shape } from 'prop-types';
 import cx from 'classnames';
 
+//Components
+import { withProfile } from 'components/HOC/withProfile';
 
 //Instruments
 import Styles from './styles.m.css';
-
+@withProfile
 export default class Like extends Component {
     static propTypes = {
         _likePost: func.isRequired,
         id:        string.isRequired,
         likes:     arrayOf(
             shape({
+                id:        string.isRequired,
                 firstName: string.isRequired,
                 lastName:  string.isRequired,
             }),
         ).isRequired,
     };
 
-    constructor () {
-        super();
-
-        this._getLikedByMe = this._getLikedByMe.bind(this);
-        this._getLikeStyles = this._getLikeStyles.bind(this);
-        this._likePost = this._likePost.bind(this);
-        this._showLikers = this._showLikers.bind(this);
-        this._hideLikers = this._hideLikers.bind(this);
-        this._getLikersList = this._getLikersList.bind(this);
-        this._getLikesDescription = this._getLikesDescription.bind(this);
-    }
-
     state = {
         showLikers: false,
-    }
+    };
 
-    _showLikers () {
+    _showLikers = () => {
         this.setState({
             showLikers: true,
         });
-    }
+    };
 
-    _hideLikers () {
+    _hideLikers = () => {
         this.setState({
             showLikers: false,
         });
-    }
+    };
 
-    _likePost () {
+    _likePost = () => {
         const { _likePost, id } = this.props;
 
         _likePost(id);
-    }
+    };
 
-    _getLikedByMe () {
+    _getLikedByMe = () => {
         const { currentUserFirstName, currentUserLastName, likes } = this.props;
 
-        return likes.some(({ firstName, lastName}) => {
-            return (
-                `${firstName} ${lastName}` === `${currentUserFirstName} ${currentUserLastName}`
-            );
+        return likes.some(({ firstName, lastName }) => {
+            return `${firstName} ${lastName}` === `${currentUserFirstName} ${currentUserLastName}`;
         });
-    }
+    };
 
-    _getLikeStyles () {
+    _getLikeStyles = () => {
         const likedByMe = this._getLikedByMe();
 
         return cx(Styles.icon, {
             [ Styles.liked ]: likedByMe,
         });
-    }
+    };
 
-    _getLikersList () {
+    _getLikersList = () => {
         const { showLikers } = this.state;
         const { likes } = this.props;
 
-        const likesJSX = likes.map(({ firstName, lastName, id}) => (
+        const likesJSX = likes.map(({ firstName, lastName, id }) => (
             <li key = { id }>{`${firstName} ${lastName}`}</li>
         ));
 
         return likes.length && showLikers ? <ul>{likesJSX}</ul> : null;
-    }
+    };
 
-    _getLikesDescription () {
+    _getLikesDescription = () => {
         const { likes, currentUserFirstName, currentUserLastName } = this.props;
 
         const likedByMe = this._getLikedByMe();
@@ -96,26 +85,26 @@ export default class Like extends Component {
         }
 
         return likes.length;
-    }
+    };
 
-    render () {
+    render() {
         const likeStyles = this._getLikeStyles();
         const likersList = this._getLikersList();
         const likesDescription = this._getLikesDescription();
 
         return (
-            <section className = { Styles.like } >
+            <section className = { Styles.like }>
                 <span
                     className = { likeStyles }
-                    onClick = { this._likePost } >
+                    onClick = { this._likePost }>
                     Like
                 </span>
                 <div>
-                    { likersList }
+                    {likersList}
                     <span
                         onMouseEnter = { this._showLikers }
                         onMouseLeave = { this._hideLikers }>
-                        { likesDescription }
+                        {likesDescription}
                     </span>
                 </div>
             </section>
